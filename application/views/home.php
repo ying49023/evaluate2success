67cbb440aaefd5b5e6710ca4941ea1719f010ae6
Sutthipol2n Khamsuwan <Sutthipol2n Khamsuwan>
2016-05-26 13:49:46 +0700

    install framework

---
+++ application/views/home.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>haha</title>
+</head>
+<body>
+555
+</body>
+</html>
