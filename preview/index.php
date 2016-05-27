90966edfffe52dd270e6e2e651f8690579d1aa18
Prasukrit TH <palm.ricing@hotmail.com>
2016-05-27 18:53:45 +0700

    แก้ไขบางส่วน

--- preview/index.php
+++ preview/index.php
@@ -56,9 +56,10 @@
             <!-- Left side column. contains the logo and sidebar -->
             <?php include './sidebarpart.php'; ?>
 
-            <!-- Content Wrapper. Contains page content -->
+            <!-- Content Wrapper. Contains page content แก้เนื้อหาแต่ละหน้าตรงนี้นะ -->
             <div class="content-wrapper">
-                <!-- Content Header (Page header) -->
+                
+                <!-- Content Header (Page header)  -->
                 <section class="content-header">
                     <h1>
                         Dashboard
@@ -70,6 +71,7 @@
                     </ol>
                     <hr>
                 </section>
+                <!--/Page header -->
 
                 <!-- Main content -->
                 <div class="row box-padding">
@@ -118,8 +120,6 @@
                             </form>
                         </div>
                     </div>
-
-
                 </div>
                 <div class="row box-padding">
                     <div class="col-md-8">
@@ -142,237 +142,45 @@
                                     var salesChartData = {
                                         labels: ["January", "February", "March", "April", "May", "June", "July"],
                                         datasets: [
-                                          {
-                                            label: "Electronics",
-                                            fillColor: "rgb(210, 214, 222)",
-                                            strokeColor: "rgb(210, 214, 222)",
-                                            pointColor: "rgb(210, 214, 222)",
-                                            pointStrokeColor: "#c1c7d1",
-                                            pointHighlightFill: "#fff",
-                                            pointHighlightStroke: "rgb(220,220,220)",
-                                            data: [65, 59, 80, 81, 56, 55, 40]
-                                          },
-                                          {
-                                            label: "Digital Goods",
-                                            fillColor: "rgba(60,141,188,0.9)",
-                                            strokeColor: "rgba(60,141,188,0.8)",
-                                            pointColor: "#3b8bba",
-                                            pointStrokeColor: "rgba(60,141,188,1)",
-                                            pointHighlightFill: "#fff",
-                                            pointHighlightStroke: "rgba(60,141,188,1)",
-                                            data: [28, 48, 40, 19, 86, 27, 90]
-                                          }
-    ]
-  };
+                                            {
+                                                label: "Electronics",
+                                                fillColor: "rgb(210, 214, 222)",
+                                                strokeColor: "rgb(210, 214, 222)",
+                                                pointColor: "rgb(210, 214, 222)",
+                                                pointStrokeColor: "#c1c7d1",
+                                                pointHighlightFill: "#fff",
+                                                pointHighlightStroke: "rgb(220,220,220)",
+                                                data: [65, 59, 80, 81, 56, 55, 40]
+                                            },
+                                            {
+                                                label: "Digital Goods",
+                                                fillColor: "rgba(60,141,188,0.9)",
+                                                strokeColor: "rgba(60,141,188,0.8)",
+                                                pointColor: "#3b8bba",
+                                                pointStrokeColor: "rgba(60,141,188,1)",
+                                                pointHighlightFill: "#fff",
+                                                pointHighlightStroke: "rgba(60,141,188,1)",
+                                                data: [28, 48, 40, 19, 86, 27, 90]
+                                            }
+                                        ]
+                                    };
                                 </script>
                                 <!-- /.chart-responsive -->
                             </div>
                         </div>
                     </div>
                 </div>
-
-
+                
                 <!-- /.content -->
             </div>
             <!-- /.content-wrapper -->
-            <footer class="main-footer">
-                <div class="pull-right hidden-xs">
-                    <b>Version</b> 1.0.0
-                </div>
-                <strong>Copyright &copy; 2016 <a href="http://almsaeedstudio.com">SIT KMUTT</a>.</strong> All rights
-                reserved.
-            </footer>
+
+            <!--Footer -->
+            <?php include './footer.php'; ?>
 
             <!-- Control Sidebar -->
-            <aside class="control-sidebar control-sidebar-dark">
-                <!-- Create the tabs -->
-                <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-                    <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-                    <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-                </ul>
-                <!-- Tab panes -->
-                <div class="tab-content">
-                    <!-- Home tab content -->
-                    <div class="tab-pane" id="control-sidebar-home-tab">
-                        <h3 class="control-sidebar-heading">Recent Activity</h3>
-                        <ul class="control-sidebar-menu">
-                            <li>
-                                <a href="javascript:void(0)">
-                                    <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-
-                                    <div class="menu-info">
-                                        <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-
-                                        <p>Will be 23 on April 24th</p>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)">
-                                    <i class="menu-icon fa fa-user bg-yellow"></i>
-
-                                    <div class="menu-info">
-                                        <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
-
-                                        <p>New phone +1(800)555-1234</p>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)">
-                                    <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
-
-                                    <div class="menu-info">
-                                        <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
-
-                                        <p>nora@example.com</p>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)">
-                                    <i class="menu-icon fa fa-file-code-o bg-green"></i>
-
-                                    <div class="menu-info">
-                                        <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
-
-                                        <p>Execution time 5 seconds</p>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                        <!-- /.control-sidebar-menu -->
-
-                        <h3 class="control-sidebar-heading">Tasks Progress</h3>
-                        <ul class="control-sidebar-menu">
-                            <li>
-                                <a href="javascript:void(0)">
-                                    <h4 class="control-sidebar-subheading">
-                                        Custom Template Design
-                                        <span class="label label-danger pull-right">70%</span>
-                                    </h4>
-
-                                    <div class="progress progress-xxs">
-                                        <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)">
-                                    <h4 class="control-sidebar-subheading">
-                                        Update Resume
-                                        <span class="label label-success pull-right">95%</span>
-                                    </h4>
-
-                                    <div class="progress progress-xxs">
-                                        <div class="progress-bar progress-bar-success" style="width: 95%"></div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)">
-                                    <h4 class="control-sidebar-subheading">
-                                        Laravel Integration
-                                        <span class="label label-warning pull-right">50%</span>
-                                    </h4>
-
-                                    <div class="progress progress-xxs">
-                                        <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)">
-                                    <h4 class="control-sidebar-subheading">
-                                        Back End Framework
-                                        <span class="label label-primary pull-right">68%</span>
-                                    </h4>
-
-                                    <div class="progress progress-xxs">
-                                        <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                        <!-- /.control-sidebar-menu -->
-
-                    </div>
-                    <!-- /.tab-pane -->
-                    <!-- Stats tab content -->
-                    <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
-                    <!-- /.tab-pane -->
-                    <!-- Settings tab content -->
-                    <div class="tab-pane" id="control-sidebar-settings-tab">
-                        <form method="post">
-                            <h3 class="control-sidebar-heading">General Settings</h3>
-
-                            <div class="form-group">
-                                <label class="control-sidebar-subheading">
-                                    Report panel usage
-                                    <input type="checkbox" class="pull-right" checked>
-                                </label>
-
-                                <p>
-                                    Some information about this general settings option
-                                </p>
-                            </div>
-                            <!-- /.form-group -->
-
-                            <div class="form-group">
-                                <label class="control-sidebar-subheading">
-                                    Allow mail redirect
-                                    <input type="checkbox" class="pull-right" checked>
-                                </label>
-
-                                <p>
-                                    Other sets of options are available
-                                </p>
-                            </div>
-                            <!-- /.form-group -->
-
-                            <div class="form-group">
-                                <label class="control-sidebar-subheading">
-                                    Expose author name in posts
-                                    <input type="checkbox" class="pull-right" checked>
-                                </label>
-
-                                <p>
-                                    Allow the user to show his name in blog posts
-                                </p>
-                            </div>
-                            <!-- /.form-group -->
-
-                            <h3 class="control-sidebar-heading">Chat Settings</h3>
-
-                            <div class="form-group">
-                                <label class="control-sidebar-subheading">
-                                    Show me as online
-                                    <input type="checkbox" class="pull-right" checked>
-                                </label>
-                            </div>
-                            <!-- /.form-group -->
-
-                            <div class="form-group">
-                                <label class="control-sidebar-subheading">
-                                    Turn off notifications
-                                    <input type="checkbox" class="pull-right">
-                                </label>
-                            </div>
-                            <!-- /.form-group -->
-
-                            <div class="form-group">
-                                <label class="control-sidebar-subheading">
-                                    Delete chat history
-                                    <a href="javascript:void(0)" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
-                                </label>
-                            </div>
-                            <!-- /.form-group -->
-                        </form>
-                    </div>
-                    <!-- /.tab-pane -->
-                </div>
-            </aside>
-            <!-- /.control-sidebar -->
+            <?php include './Controlsidebar.php'; ?>
+                        
             <!-- Add the sidebar's background. This div must be placed
                  immediately after the control sidebar -->
             <div class="control-sidebar-bg"></div>
