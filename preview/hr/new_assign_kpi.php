8b36dd74298be6c226ec65098cd767723d90b743
Prasukrit TH <palm.ricing@hotmail.com>
2016-07-30 22:39:07 +0700

    update เพิ่มเติม

--- preview/hr/new_assign_kpi.php
+++ preview/hr/new_assign_kpi.php
@@ -1,8 +1,12 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include('./classes/connection_mysqli.php');
 
+if(isset($_GET["emp_id"])){
+    $get_emp_id = $_GET["emp_id"];
+}
+
+$sql_assign_kpi = "INSERT INTO kpi_responsible (kpi_responsible_id, evaluate_employee_id, kpi_id, goal, percent_weight, success, due_date) 
+VALUES (DEFAULT,(SELECT evaluate_employee_id FROM evaluation_employee WHERE employee_id = '".$get_emp_id."' AND evaluation_code = 1),15,10,20,0,'0000-00-00')"
+
+?>
