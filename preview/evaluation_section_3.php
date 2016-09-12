05fd8ac10e2cd57fd8934282c6ecd387507edb7f
Prasukrit TH <palm.ricing@hotmail.com>
2016-09-12 14:41:10 +0700

    update jaaa

--- preview/evaluation_section_3.php
+++ preview/evaluation_section_3.php
@@ -24,6 +24,24 @@
         $msg='';
         
         include './classes/connection_mysqli.php';
+        
+        if(isset($_POST["submit_comment"])){
+            $comment1 = '';
+            if(isset($_POST["comment1"])){
+                $comment1 = $_POST["comment1"];
+            }
+            $comment2 = '';
+            if(isset($_POST["comment2"])){
+                $comment2 = $_POST["comment2"];
+            }
+            $comment3 = '';
+            if(isset($_POST["comment3"])){
+                $comment3 = $_POST["comment3"];
+            }
+            $sql_insert_com = "INSERT INTO evaluation_comment(evaluate_comment_id, comment1, comment2, comment3, evaluate_employee_id) 
+                                VALUES (DEFAULT,'$comment1','$comment2','$comment3',9)";
+            
+        }
             
 ?>
 <!DOCTYPE html>
@@ -224,7 +242,63 @@
                             </table>
                             </form>
                             <br>
-                             <table class="table table-hover">
+                            <div class="row">
+                                <div class="box-padding">
+                                    <h4><b>3.2 การพิจารณาความดี ความชอบ และการลงโทษทางวินัย</b></h4>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6 box-padding-table">
+                                    
+                                    <table class="table table-bordered">
+                                        <tr>
+                                            <td colspan=2><b><u>ประวัติการได้รับรางวัล / ยกย่อง</u></b></td>
+                                        </tr>
+                                                <?php
+                                                $sql_reward = "SELECT *
+                                                    FROM evaluatation_penalty_reward epr 
+                                                    JOIN penalty_reward pr ON epr.penalty_reward_id=pr.penalty_reward_id
+                                                    WHERE evaluate_employee_id='$get_eval_emp_id' AND pr.penalty_reward_indicated=1";
+                                                $query_reward = mysqli_query($conn, $sql_reward);
+                                                $i = 0;
+                                                while ($reault_reward = mysqli_fetch_array($query_reward, MYSQLI_ASSOC)) {
+                                                    $i++;
+                                                    ?>
+                                        <tr >
+                                            <th style="padding: 10px;" scope="row" style="width: 25px;" ><?php echo $i; ?></th>
+                                            <td><input type="text" name="reward" size="75" value="<?php echo $reault_reward["penalty_reward_name"]; ?>" readonly></td>
+                                        </tr>
+                                                    <?php
+                                                }
+                                                ?>
+                                </table>
+                                </div>
+                                <div class="col-md-6 box-padding-table">
+                                    <table class="table table-bordered">
+                                        <tr>
+                                            <td colspan=2><b><u>ประวัติการลงโทษทางวินัย</u></b></td>
+                                        </tr>
+                                                <?php
+                                                $sql_reward = "SELECT *
+                                                    FROM evaluatation_penalty_reward epr 
+                                                    JOIN penalty_reward pr ON epr.penalty_reward_id=pr.penalty_reward_id
+                                                    WHERE evaluate_employee_id='$get_eval_emp_id' AND pr.penalty_reward_indicated=0";
+                                                $query_reward = mysqli_query($conn, $sql_reward);
+                                                $i = 0;
+                                                while ($reault_reward = mysqli_fetch_array($query_reward, MYSQLI_ASSOC)) {
+                                                    $i++;
+                                                    ?>
+                                        <tr>
+                                            <th style="padding: 10px;" scope="row" style="width: 25px;" ><?php echo $i; ?></th>
+                                            <td><input type="text" name="reward" size="75" value="<?php echo $reault_reward["penalty_reward_name"]; ?>" readonly></td>
+                                        </tr>
+                                                    <?php
+                                                }
+                                                ?>
+                                </table>
+                                </div>
+                            </div>
+<!--                             <table class="table table-hover">
                                 <thead class="thead-inverse">
                                     <tr>
                                         <th colspan="6"><h4><b>3.2 การพิจารณาความดี ความชอบ และการลงโทษทางวินัย</b></h4></th>
@@ -236,12 +310,15 @@
                                         <td colspan=2><b><u>ประวัติการได้รับรางวัล / ยกย่อง</u></b></td>
                                         <td colspan=2><b><u>ประวัติการลงโทษทางวินัย</u></b></td>
                                     </tr>
+                                    
                                     <tr>
-                                       <th scope="row">1</th>
-                                       <td><input type="text" name="reward" size="40" readonly></td>
+                                
+                                       <th scope="row"><?php echo $i; ?></th>
+                                       <td><input type="text" name="reward" size="40" value="<?php echo $reault_reward["penalty_reward_name"]; ?>" readonly></td>
                                        <th scope="row">1</th>
                                        <td><input type="text" name="penalty" size="40" readonly></td>
                                     </tr>
+                                    
                                      <tr>
                                        <th scope="row">2</th>
                                        <td><input type="text" name="reward" size="40" readonly></td>
@@ -280,35 +357,66 @@
                                     </tr>
 
                                 </tbody>  
-                        </table>
-                        <table>
-                                <tbody>
+                        </table>-->
+                            <div class="row">
+                                <form action="" method="post">
+                                <div class="col-md-6">
+                                    <table class="table table-bordered">
+                                        <tbody>
 
-                                    <tr>
-                                        <td colspan= 2><b>ความคิดเห็นของผู้บังคับบัญชาตามสายงาน (ตามข้อเท็จจริง ตามข้อ 3.1, 3.2)</b></td>
-                                        <td align="center" height="80" width="100" style="background-color:#E6E6FA;"><b>คะแนนที่ได้รับเท่ากับ</b></td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td><input type="text" name="commentpart3" size="60"></td>
-                                        <td align="center" height="50" width="100" style="background-color:#F5F5F5;"><input type="number" name="point" min="0.5" max="60" disabled readonly></td>
+                                            <tr>
+                                                <td colspan= 2 ><b>ความคิดเห็นของผู้บังคับบัญชาตามสายงาน (ตามข้อเท็จจริง ตามข้อ 3.1, 3.2)</b></td>
+                                                <td align="center" style="background-color:#E6E6FA;"><b>คะแนนที่ได้รับเท่ากับ</b></td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row" style="width: 20px;">1</th>
+                                                <td ><input type="text" name="commentpart3" size="60"></td>
+                                                <?php
+                                                $sql_get_score = "SELECT point_leave
+                                                                FROM evaluation_employee
+                                                                WHERE evaluate_employee_id='$get_eval_emp_id'";
+                                                $query_get_score = mysqli_query($conn, $sql_get_score);
+                                                while($result_get_score = mysqli_fetch_array($query_get_score,MYSQLI_ASSOC)){
+                                                ?>
+                                                <td align="center"   style="background-color:#F5F5F5;width: 80px;"><input type="number" name="point" min="0.5" max="60" value="<?php echo $result_get_score["point_leave"]; ?>" disabled readonly></td>
+                                                <?php } ?>
+                                            </tr>
+                                             <tr>
+                                                <th scope="row">2</th>
+                                                <td><input type="text" name="commentpart3" size="60"></td>
+                                                <td align="center"  style="background-color:#E6E6FA;"><b>คะแนนเต็ม (ระหว่าง 5-20)</b></td>
 
-                                    </tr>
-                                     <tr>
-                                        <th scope="row">2</th>
-                                        <td><input type="text" name="commentpart3" size="60"></td>
-                                        <td align="center" height="80" width="100" style="background-color:#E6E6FA;"><b>คะแนนเต็ม (ระหว่าง 5-20)</b></td>
+                                            </tr>
+                                             <tr>
+                                                 <?php
+                                                 $sql_sum_score = "SELECT sum_point_leave
+                                                                    FROM evaluation_sumpoint
+                                                                    WHERE position_level_id=( 	SELECT position_level_id
+                                                                    FROM employees e 
+                                                                    JOIN evaluation_employee ee ON e.employee_id=ee.employee_id
+                                                                                                                    WHERE ee.evaluate_employee_id='$get_eval_emp_id')
+                                                                    ";
+                                                 $query_sum_score = mysqli_query($conn, $sql_sum_score);
+                                                 while($result_sum_score = mysqli_fetch_array($query_sum_score,MYSQLI_ASSOC)){
+                                                 ?>
+                                                <th scope="row">3</th>
+                                                <td><input type="text" name="commentpart3" size="60"></td>
+                                                <td align="center"  style="background-color:#F5F5F5;"><input type="number" name="point" min="5" max="20" value="<?php echo $result_sum_score["sum_point_leave"]; ?>" disabled readonly></td>
+                                                 <?php } ?>
+                                             </tr>
 
-                                    </tr>
-                                     <tr>
-                                        <th scope="row">3</th>
-                                        <td><input type="text" name="commentpart3" size="60"></td>
-                                        <td align="center" height="50" width="100" style="background-color:#F5F5F5;"><input type="number" name="point" min="5" max="20" disabled readonly></td>
-                                    </tr>
-                                
-                                
-                                </tbody>
-                          </table>
+
+                                        </tbody>
+                                        
+                                  </table>
+                                    <div class="form-group">
+                                        <input class="btn btn-success" type="submit" name="submit_comment" value="บันทึก">
+                                        <input class="btn btn-danger" type="reset" name="reset" value="รีเซ็ต" >
+                                    </div>
+                                </div>
+                                </form>
+                            </div>
+                            
                                         
                         </div>           
                     </div> 
